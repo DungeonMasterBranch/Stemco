216010e0fcb0d567d482e68c537df91f542d15d5
seniorasl <aslankaan560@gmail.com>
2022-04-29 00:35:59 +0600

    На доработку Дастану Верификация, прверка верификации в профиле, изменение данных пользователя.

--- app/src/main/java/com/example/quizme/ProfileFragment.java
+++ app/src/main/java/com/example/quizme/ProfileFragment.java
@@ -1,13 +1,20 @@
 package com.example.quizme;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
     @Nullable
@@ -15,4 +22,40 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile,container,false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser vUser = mAuth.getCurrentUser();
+
+//        vUser.reload();
+//        vUser = mAuth.getCurrentUser();
+
+
+        EditText nameEdit, emailEdit;
+        TextView editBtn;
+        Button verifyBtn;
+        nameEdit = getView().findViewById(R.id.nameEdit);
+        //String passEdit = getView().findViewById(R.id.passwordEdit);
+        emailEdit = getView().findViewById(R.id.emailEdit);
+        editBtn = getView().findViewById(R.id.editBtn);
+        verifyBtn = getView().findViewById(R.id.verifyBtn);
+        //code for check and send verification;
+
+        emailEdit.setText(vUser.getEmail());
+
+            if (!vUser.isEmailVerified()){
+                verifyBtn.setVisibility(View.VISIBLE);
+            } else {
+                verifyBtn.setVisibility(View.INVISIBLE);
+            }
+
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/quizme/SignupActivity.java
+++ app/src/main/java/com/example/quizme/SignupActivity.java
@@ -4,14 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -74,6 +78,21 @@ public class SignupActivity extends AppCompatActivity {
                                     "Registration successful!",
                                     Toast.LENGTH_LONG)
                                     .show();
+
+                            //mAuth.signInWithEmailAndPassword(email, password);
+
+                            mAuth.getCurrentUser().sendEmailVerification()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Toast.makeText(getApplicationContext() , "Email verification has been sent",Toast.LENGTH_SHORT).show();
+                                        }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.i("TAG", "Fail to sent verification link");
+                                }
+                            });
 
                             mAuth.signInWithEmailAndPassword(email,password);
                             // if the user created intent to login activity
